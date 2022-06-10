be6010d940f1856b9523426e162c31c4ca2648ef
Zander Ellis <zander.ellis@wearephlo.com>
2022-06-10 15:43:44 +0100

    tutorial 1 completed

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Nav/Nav";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
